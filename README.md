4877fadd1b03ebecf8ca37ff84848a68357c03f5
Eli Filiba <e.filiba@abysssolutions.com.au>
2021-09-04 14:10:15 +1000

    Add timing and start optimisation / progress table

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # rust-prime-sieve
-Rust version of prime sieve algorithm
+Rust version of prime sieve algorithm from [Dave's Garage](https://www.youtube.com/channel/UCNzszbnvQeFzObW0ghk0Ckw) YouTube channel
 
 Sample code from https://www.youtube.com/watch?v=D3h62rgewZM implemented in Rust.
+
+## Code optimisation stages
+
+### Results from the YouTube video that first stage (1: Base ) is based on
+- Python   25
+- C#     2651
+- C++    4645
+  
+#### Run on MacBook 2018 - 2.2 GHz 6-Core Intel Core i7
+
+| Stage    | Passes (debug) | Passes (optimised) | Comments                                            |
+|   ---    |     :---:      |       :---:        |                         ---                         |
+| 1: Base  |       62       |       4900         | Uses a full array 1 million and 1 for easy indexing |
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-fn main() {
-  let sieve_size = usize::pow(10, 3);
-  let mut sieve = vec![true; sieve_size + 1]; // Add extra 1 as 0 ignored (positions used as the number)
+use std::time::{Instant, Duration};
 
-  run_sieve(&mut sieve);
-  print_results(sieve, false);
+fn main() {
+  let sieve_size = usize::pow(10, 6); // Upper limit - One million
+  let duration = Duration::new(5, 0); // Five seconds
+  let mut passes = 0;
+  let start = Instant::now();
+
+  let mut sieve = Vec::new();
+  while Instant::now() - start < duration {
+    sieve = vec![true; sieve_size + 1]; // Add extra 1 as 0 ignored (positions used as the number)
+    run_sieve(&mut sieve);
+    passes += 1;
+  }
+  print_results(sieve, Instant::now() - start, passes, sieve_size, false);
 }
 
-// Number of primes by 10 to the power of index. e.g. 1000 = 10^4 => value at index 4 is 168 (168 prime numbers less than 1000)
+// Number of primes by 10 to the power of index. e.g. 1 000 000 = 10^6 => value at index 6 is 78498
 const PRIME_COUNTS: [usize; 9] = [0, 4, 25, 168, 1229, 9592, 78498, 664579, 5761455];
 
 fn run_sieve(sieve: &mut Vec<bool>) {
@@ -34,22 +43,32 @@ fn run_sieve(sieve: &mut Vec<bool>) {
   }
 }
 
-fn print_results(sieve: Vec<bool>, show_results: bool) {
+fn print_results(sieve: Vec<bool>, duration: Duration, passes: usize, sieve_size: usize, show_results: bool) {
   if show_results {
     print!("2, ");
   }
 
   let mut count = 1;
-  for i in (3..sieve.len()).step_by(2) {
+  for i in (3..sieve_size).step_by(2) {
     if sieve[i] {
       if show_results {
-          print!("{:?}, ", i);
+        print!("{:?}, ", i);
       }
       count += 1;
     }
   }
 
-  println!("\nresult: {:?}", count);
+  if show_results {
+    println!("");
+  }
+
+  let verified_message = if validate_results(sieve, sieve_size) { "verified" } else { "incorrect" };
+  println!("{} passes in {:?}, upper limit: {}, result: {} ({})", passes, duration, sieve_size, count, verified_message);
+}
+
+fn validate_results(sieve: Vec<bool>, sieve_size: usize) -> bool {
+  let power = f32::log(sieve_size as f32, 10.0) as usize;
+  count_set_sieve(sieve) == PRIME_COUNTS[power]
 }
 
 fn count_set_sieve(sieve: Vec<bool>) -> usize {
@@ -64,11 +83,11 @@ fn count_set_sieve(sieve: Vec<bool>) -> usize {
 
 #[test]
 fn should_return_the_correct_number_of_primes_at_each_base_10() {
-  for i in 1..7 {
+  for i in 1..9 {
     let sieve_size = usize::pow(10, i);
     let mut sieve = vec![true; sieve_size + 1];
   
     run_sieve(&mut sieve);
-    assert_eq!(count_set_sieve(sieve), PRIME_COUNTS[i as usize]);
+    assert!(validate_results(sieve, sieve_size));
   }
 }
